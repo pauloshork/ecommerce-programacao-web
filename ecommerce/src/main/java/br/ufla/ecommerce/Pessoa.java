5dead904568f4a8bb13369bfe3fc9870da0645a2
Paulo Marcondes <paulovitormsh@hotmail.com>
2015-06-09 22:34:07 -0300

    Importação do inicio do projeto. Ele contem a configuração de dependências no maven; Arquivos de configuração para o spring, hibernate, wicket; As classes do diagrama mapeadas com o hibernate.

---
+++ ecommerce/src/main/java/br/ufla/ecommerce/Pessoa.java
@@ -0,0 +1,73 @@
+package br.ufla.ecommerce;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 7928292132802263105L;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Id
+	@Column(name="cpf")
+	private BigInteger cpf;
+	
+	@Column(name="endereco")
+	private String endereco;
+	
+	@Column(name="telefone")
+	private String telefone;
+	
+	@OneToOne(cascade=CascadeType.ALL, optional=false, fetch=FetchType.EAGER, mappedBy="usuario")
+	private Usuario usuario;
+	
+	public Pessoa() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigInteger getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(BigInteger cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
