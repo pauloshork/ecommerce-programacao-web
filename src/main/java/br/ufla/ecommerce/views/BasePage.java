8b4e8921ec991ac1c59ba5e4231a2baccc225dd3
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2015-06-11 19:27:04 -0300

    Correção dos conflitos

--- src/main/java/br/ufla/ecommerce/views/BasePage.java
+++ src/main/java/br/ufla/ecommerce/views/BasePage.java
@@ -8,15 +8,12 @@ import java.util.Map;
 import org.apache.wicket.markup.head.CssContentHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
-<<<<<<< HEAD
 import org.apache.wicket.markup.html.IPackageResourceGuard;
 import org.apache.wicket.markup.html.PackageResourceGuard;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-=======
 import org.apache.wicket.markup.html.WebPage;
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.CssResourceReference;
@@ -41,14 +38,11 @@ public class BasePage extends WebPage {
 	 * Mapa de arquivos de imagem que serão usados pela página.
 	 */
 	private Map<String, String> imgFiles;
-<<<<<<< HEAD
 	
 	/**
 	 * Controlador de permissões dos arquivos.
 	 */
 	private static IPackageResourceGuard resourceGuard = new PackageResourceGuard();
-=======
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 
 	/**
 	 * Inicia a página base.
@@ -62,7 +56,6 @@ public class BasePage extends WebPage {
 		cssFiles = new ArrayList<String>();
 		jsFiles = new ArrayList<String>();
 		imgFiles = new HashMap<String, String>();
-<<<<<<< HEAD
 		
 		try {
 			// Checa quais arquivos devem ser carregados com a página.
@@ -100,17 +93,6 @@ public class BasePage extends WebPage {
 	}
 	
 	/**
-=======
-		
-		
-		// Checa quais arquivos devem ser carregados com a página.
-		requestExternalFiles();
-		// Cria o link das imagens com o HTML
-		prepareImages();
-    }
-	
-	/**
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 	 * Função que deverá ser implementada pela página filha, caso ela necessite
 	 * de algum arquivo externo que não foi incluido na página base.
 	 * 
@@ -124,13 +106,10 @@ public class BasePage extends WebPage {
 		addJsFile("js/bootstrap.min.js");
 		addJsFile("js/base.js");
 		
-<<<<<<< HEAD
 		// addImg("logo", "imgs/logo.png");
 		
 		// addPermission("fonts/glyphicons-halflings-regular.woff2");
-=======
 		//addImg("logo", "imgs/logo.png");
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 	}
 	
 	/**
@@ -146,11 +125,8 @@ public class BasePage extends WebPage {
 	
 	/**
 	 * Adiciona um arquivo CSS que deve ser carregado com a página.
-<<<<<<< HEAD
 	 * @param cssFile caminho do arquivo que sera carregado.
-=======
 	 * @param cssFile diretorio do arquivo que sera carregado.
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 	 */
 	public void addCssFile(String cssFile) {
 		cssFiles.add(cssFile);
@@ -158,11 +134,8 @@ public class BasePage extends WebPage {
 	
 	/**
 	 * Adiciona um arquivo JavaScript que deve ser carregado com a página.
-<<<<<<< HEAD
 	 * @param jsFile caminho do arquivo que sera carregado.
-=======
 	 * @param jsFile diretorio do arquivo que sera carregado.
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 	 */
 	public void addJsFile(String jsFile) {
 		jsFiles.add(jsFile);
@@ -171,17 +144,13 @@ public class BasePage extends WebPage {
 	/**
 	 * Adiciona uma imagem que deve ser carregada com a página.
 	 * @param nome dado para a imagem, para linkar com o arquivo HTML.
-<<<<<<< HEAD
 	 * @param imgFile caminho da imagem que sera carregada.
-=======
 	 * @param imgFile diretorio da imagem que sera carregada.
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 	 */
 	public void addImg(String resource, String imgFile) {
 		imgFiles.put(resource, imgFile);
 	}
 	
-<<<<<<< HEAD
 	/**
 	 * Adiciona permissão de acesso a um arquivo.
 	 * @param file caminho para o arquivo que receberá permissão de acesso.
@@ -191,31 +160,25 @@ public class BasePage extends WebPage {
 		resource = "br/ufla/ecommerce/views/"+resource;
 		resourceGuard.accept(BasePage.class, resource);
 	}
-	
-=======
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
+
+
 	@Override
 	public void renderHead(IHeaderResponse response) {
 		super.renderHead(response);
 		
-<<<<<<< HEAD
 		
 		// renderiza todos os arquivos JS
 		for (String jsFile : jsFiles) {
 			resourceGuard.accept(BasePage.class, "br/ufla/ecommerce/views/"+jsFile);
-=======
+		}
 		// renderiza todos os arquivos JS
 		for (String jsFile : jsFiles) {
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 			JavaScriptResourceReference file = new JavaScriptResourceReference(BasePage.class, jsFile);
 			response.render(JavaScriptReferenceHeaderItem.forReference(file));
 		}
 		// renderiza todos os arquivos CSS
 		for (String cssFile : cssFiles) {
-<<<<<<< HEAD
 			resourceGuard.accept(BasePage.class, "br/ufla/ecommerce/views/"+cssFile);
-=======
->>>>>>> f6380af1e0439c9e9294eac24dd339a3f4c01124
 			CssResourceReference file = new CssResourceReference(BasePage.class, cssFile);
 			response.render(CssContentHeaderItem.forReference(file));
 		}
